cbd640b86187d447d60ddbcd42d568e3bda67ed9
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-06-17 00:01:02 -0700

    feat: add methods to MessagesController for handling GET and POST requests

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  getMessages() {
+    return;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return id;
+  }
+
+  @Post('/')
+  createMessage(@Body() body: any) {
+    return body;
+  }
+}
