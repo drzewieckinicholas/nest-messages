ea0d0e982452c7d2c1ab6fab8857dfb37de77b7d
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-06-17 15:02:09 -0700

    feat: throw a NotFoundException on invalid message id
    N

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,25 +1,44 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
+  /**
+   * Retrieves a message by id.
+   * @param id The id of the message to retrieve.
+   * @returns The message with the specified id.
+   * @throws NotFoundException if the message with the specified id is not found.
+   */
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException(`Message with id ${id} not found`);
+    }
+  }
+
   /**
    * Retrieves all messages.
    * @returns An array of messages.
    */
   @Get()
   getMessages() {
-    return;
-  }
-
-  /**
-   * Retrieves a message by ID.
-   * @param id The ID of the message to retrieve.
-   * @returns The message with the specified ID.
-   */
-  @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return id;
+    return this.messagesService.findAll();
   }
 
   /**
@@ -27,8 +46,8 @@ export class MessagesController {
    * @param body The message to create.
    * @returns The created message.
    */
-  @Post('/')
+  @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    return body;
+    return this.messagesService.create(body.content);
   }
 }
